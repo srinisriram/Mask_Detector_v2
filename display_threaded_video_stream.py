c62758fd70d906ffc955d8a49f68c618611a9612
srinisriram <srinivassriram06@gmail.com>
2021-04-18 14:50:29 -0400

    Threaded Video Stream - USB Cam functionality

--- display_threaded_video_stream.py
+++ display_threaded_video_stream.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from __future__ import print_function
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
@@ -7,29 +6,21 @@ import cv2
 
 display = True
 run_program = True
-# created a *threaded* video stream, allow the camera sensor to warmup,
-# and start the FPS counter
 print("[INFO] sampling THREADED frames from webcam...")
 vs = WebcamVideoStream(src=0).start()
 fps = FPS().start()
-# loop over some frames...this time using the threaded stream
 while run_program:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=320)
-    # check to see if the frame should be displayed to our screen
     if display:
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         run_program = False
-    # update the FPS counter
     fps.update()
-# stop the timer and display FPS information
 fps.stop()
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# do a bit of cleanup
+
 cv2.destroyAllWindows()
 vs.stop()
