3802f2ad4b53de318c7ef4352bc49f1de2e27202
srinisriram <srinivassriram06@gmail.com>
2021-04-18 12:21:49 -0400

    Threaded Video Stream - Fixed FPS import

--- open_camera_threading.py
+++ open_camera_threading.py
@@ -12,20 +12,20 @@ print("Setting up Video Stream")
 display = True
 vs = Threaded_Video_Stream().start()
 time.sleep(1.0)
-fps = FPS().start()
+# fps = FPS().start()
 
 while True:
 	frame = vs.read()
 	frame = imutils.resize(frame, width=320)
-	fps.update()
-	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	# fps.update()
+	# print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 	if display:
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
 
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# fps.stop()
+# print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+# print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 cv2.destroyAllWindows()
 vs.stop()
