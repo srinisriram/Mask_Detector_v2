132e07efc17b9247f19a5625db03a5acf0f2ef33
srinisriram <srinivassriram06@gmail.com>
2021-04-18 12:40:37 -0400

    Threaded Video Stream - USB Cam functionality

--- open_camera_threading.py
+++ open_camera_threading.py
@@ -1,31 +1,26 @@
-from imutils.video.pivideostream import PiVideoStream
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import argparse
 import imutils
 import time
 import cv2
 from threaded_video_capture import Threaded_Video_Stream
 from Production.frames_per_second import FPS
 
-print("Setting up Video Stream")
+print("[INFO] Started Video Stream")
 display = True
-vs = Threaded_Video_Stream().start()
-time.sleep(1.0)
-# fps = FPS().start()
+vs = Threaded_Video_Stream(src=0).start()
+fps = FPS().start()
 
 while True:
-	frame = vs.read()
-	frame = imutils.resize(frame, width=320)
-	# fps.update()
-	# print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-	if display:
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+    if display:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+    fps.update()
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# fps.stop()
-# print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-# print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 cv2.destroyAllWindows()
 vs.stop()
--- threaded_video_capture.py
+++ threaded_video_capture.py
@@ -1,19 +1,12 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 from threading import Thread
 import cv2
 
 
 class Threaded_Video_Stream:
     def __init__(self):
-        self.resolution = (320, 240)
-        self.framerate = 32
-        self.camera = PiCamera()
-        self.camera.resolution = self.resolution
-        self.camera.framerate = self.framerate
-        self.rawCapture = PiRGBArray(self.camera, size=self.resolution)
-        self.stream = self.camera.capture_continuous(self.rawCapture,
-                                                     format="bgr", use_video_port=True)
+        self.src = 0
+        self.stream = cv2.VideoCapture(self.src)
+        (self.grabbed, self.frame) = self.stream.read()
         # initialize the frame and the variable used to indicate
         # if the thread should be stopped
         self.frame = None
@@ -26,18 +19,12 @@ class Threaded_Video_Stream:
 
     def update(self):
         # keep looping infinitely until the thread is stopped
-        for f in self.stream:
-            # grab the frame from the stream and clear the stream in
-            # preparation for the next frame
-            self.frame = f.array
-            self.rawCapture.truncate(0)
+        while True:
             # if the thread indicator variable is set, stop the thread
-            # and resource camera resources
             if self.stopped:
-                self.stream.close()
-                self.rawCapture.close()
-                self.camera.close()
                 return
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
 
     def read(self):
         # return the frame most recently read
