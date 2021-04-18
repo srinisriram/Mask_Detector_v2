a26fadc25cca10cb36c665b7a2921461e6e21c25
srinisriram <srinivassriram06@gmail.com>
2021-04-18 11:59:42 -0400

    Threaded Video Stream

---
+++ open_camera_threading.py
@@ -0,0 +1,31 @@
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video import FPS
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import imutils
+import time
+import cv2
+from threaded_video_capture import Threaded_Video_Stream
+
+print("Setting up Video Stream")
+display = True
+vs = Threaded_Video_Stream().start()
+time.sleep(1.0)
+fps = FPS().start()
+
+while True:
+	frame = vs.read()
+	frame = imutils.resize(frame, width=320)
+    fps.update()
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	if display:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+cv2.destroyAllWindows()
+vs.stop()
